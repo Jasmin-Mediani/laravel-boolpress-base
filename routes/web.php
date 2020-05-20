563e5344e059a2340b405cebedafd4bd5b03e548
Jasmin-Mediani <molihua1992@gmail.com>
2020-05-20 15:18:01 +0200

    inizio crud

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/', 'PostController@index')->name('posts.index');
+// Route::get('/', 'PostController@index')->name('posts.index');
 
-Route::get('/published', 'PostController@indexPublished')->name('posts.index');
+// Route::get('/published', 'PostController@indexPublished')->name('posts.index'); // ->name('posts.index.published')
+
+Route::get('/posts/published', 'PostController@indexPublished')->name('posts.index.published');
+
+Route::resource('posts', 'PostController');
+
